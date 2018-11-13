a4967ae6003edd1c7e5ae3a5f564fe10d4981a9a
Carlos Perrout <carlosperrout@gmail.com>
2018-11-13 13:50:31 -0200

    Getting Initial Data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,13 @@
-import { getInitialData } from '../utils/API'
+import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
-import { setAuthedUser } from '../actions/authedUser'
+import setAuthedUser from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
 
 /* == HANDLE == */
 // 1. handleInidialData
-export function handleInidialData() {
+export function handleInitialData() {
     return (dispatch) => {
         return getInitialData()
             .then(({ users, tweets }) => {
