a07904a43b4de581a30017e6ae45a3b3c6e8f00f
Carlos Perrout <carlosperrout@gmail.com>
2018-11-14 14:56:22 -0200

    Implementing Tweed-like functionality

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,4 +1,7 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 /* == ACTION CREATORS == */
 // 1. receiveTweets
@@ -6,6 +9,29 @@ export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets,
+    }
+}
+
+// 2. 
+function toggleTweet({ id, authedUser, hasLiked }) {
+    return {
+        type: TOGGLE_TWEET,
+        id,
+        authedUser,
+        hasLiked
+    }
+}
+
+export function handleToggleTweet(info) {
+    return (dispatch) => {
+        dispatch(toggleTweet(info))
+
+        return saveLikeToggle(info)
+            .catch((e) => {
+                console.warn('Error in handleToggleTweet: ', e)
+                dispatch(toggleTweet(info))
+                alert('There was an error liking the tweet. Try agian!')
+            })
     }
 }
 // ---> END ACTION CREATORS <---
